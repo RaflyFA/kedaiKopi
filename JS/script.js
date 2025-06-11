f188e9da868301740d7446455ec0c4d05d2468d9
RaflyFa <rfl.yfa@gmail.com>
2025-06-11 15:33:23 +0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// toogle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// Hmaburger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+   navbarNav.classList.toggle('active')
+}
+
+// klik diluar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+   if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+      navbarNav.classList.remove('active')
+   }
+})
